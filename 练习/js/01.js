88708d102fe0ecab2a3ad13e855631501a883141
zhangjijian <747301585@qq.com>
2018-10-26 17:12:34 +0800

    ls

--- 练习/js/01.js
+++ 练习/js/01.js
@@ -1,22 +1,22 @@
 function Testfunction(id){
-    var ele = document.getElementById(id)
-    (function(){
-        document
-    }) ();
-}
+    var ele = document.getElementById
+        ele('myP').addEventListener('click',function(){
+            alert('你点击了p1元素')
+        },false);
+    }
 
-document.getElementById("myP").addEventListener("click",function(){
-    alert("你点击了p1元素");
- },false); /**false为冒泡传递**/
-document.getElementById("myDiv").addEventListener("click",function(){
-    alert('你点击了div1元素');
- },false);
-document.getElementById("myP2").addEventListener("click",function(){
-    alert('你点击了p2元素');
- },true) ;/**true为捕获传递**/
-document.getElementById("myDiv2").addEventListener("click",function(){
-    alert('你点击了div2元素');
- },true);
+// document.getElementById("myP").addEventListener("click",function(){
+//     alert("你点击了p1元素");
+//  },false); /**false为冒泡传递**/
+// document.getElementById("myDiv").addEventListener("click",function(){
+//     alert('你点击了div1元素');
+//  },false);
+// document.getElementById("myP2").addEventListener("click",function(){
+//     alert('你点击了p2元素');
+//  },true) ;/**true为捕获传递**/
+// document.getElementById("myDiv2").addEventListener("click",function(){
+//     alert('你点击了div2元素');
+//  },true);
 
- document.getElementById('myDiv3').addEventListener('mousemove',myFunction)
+//  document.getElementById('myDiv3').addEventListener('mousemove',myFunction)
  
