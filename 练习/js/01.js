17426fadd6bb258a9be6aff04612b4494da0535c
zhangjijian <747301585@qq.com>
2018-10-27 18:42:44 +0800

    ll

--- 练习/js/01.js
+++ 练习/js/01.js
@@ -21,30 +21,37 @@
 
 //  document.getElementById('myDiv3').addEventListener('mousemove',myFunction)
  
-function add(x,y){
-    var z=x+y;
-    return z;
-}
-var i=add(15,11)
-console.log(i)
+// function add(x,y){
+//     var z=x+y;
+//     return z;
+// }
+// var i=add(15,11)
+// console.log(i)
 
-/* 动态传参 */
-// function add2(){
-//     var z = 0;
-//     if(){
 
+
+// function slogan(num,time){
+//     /* 参数的默认值*/
+//     // if (time == undefined){
+//     //     time =7;
+//     // }
+//     time = time || 8;
+//     for(var i = 1;i <= num; i++){
+//         console.log(time+'点'+ i +'学习');
 //     }
 // }
+// var time=1;
+// slogan(10);
 
-function slogan(num,time){
-    /* 参数的默认值*/
-    // if (time == undefined){
-    //     time =7;
-    // }
-    time = time || 8;
-    for(var i = 1;i <= num; i++){
-        console.log(time+'点'+ i +'学习');
+/* 动态传参 */
+function add2(){
+    var z = 0;
+    // x = arguments[0];
+    // y = arguments[1];
+    for(var i=0;i<=arguments.length;i++){
+        z += arguments[i];
     }
+    return z;
 }
-var time=1;
-slogan(10);
+var te= add2(1,2,3,4);
+console.log('ls'+ te);
